7538bc6d6ff67121b4a56ca3dfe13a00179373a3
Abhishek ✪ <72429449+abyseku@users.noreply.github.com>
2022-05-14 17:53:36 +0530

    Update 3d.js

--- assets/js/3d.js
+++ assets/js/3d.js
@@ -1,9 +1,6 @@
-alert("valan Fernandes presents 3d world")
+
 // Scene
 const scene = new THREE.Scene()
-//   Camera
-const camera =new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000)
-camera.position.z = 20
 
 //   Renderer
 const renderer = new THREE.WebGLRenderer({
