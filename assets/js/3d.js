5e5ee30c03f29b0f7e1e700013512ff75728b144
Abhishek ✪ <72429449+abyseku@users.noreply.github.com>
2022-05-14 17:59:23 +0530

    Update 3d.js

--- assets/js/3d.js
+++ assets/js/3d.js
@@ -1,6 +1,9 @@
-
 // Scene
 const scene = new THREE.Scene()
+
+//   Camera
+const camera =new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000)
+camera.position.z = 20
 
 //   Renderer
 const renderer = new THREE.WebGLRenderer({
